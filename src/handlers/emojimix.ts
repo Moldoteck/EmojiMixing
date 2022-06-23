e020f34be7d856b173ca6a0002f334bbbea4c5a5
Moldoteck <cpadureac@gmail.com>
2022-06-23 22:59:38 +0300

    add reversed cache to search any possible pair, add multiple querry responses for too many options

--- src/handlers/emojimix.ts
+++ src/handlers/emojimix.ts
@@ -3,6 +3,7 @@ import { Context } from 'telegraf'
 import { cacheObject } from './cache'
 import { emojidata } from './emojiData'
 import { knownSupportedDates } from './emojis'
+import { revCacheObject } from './revcache'
 const needle = require('needle')
 
 let API = 'https://www.gstatic.com/android/keyboard/emojikitchen/'
@@ -10,7 +11,20 @@ let API = 'https://www.gstatic.com/android/keyboard/emojikitchen/'
 // let regex = new RegExp(
 //   '(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])'
 // )
-
+const splitChunk = (arr, size) => {
+  const res = [];
+  for(let i = 0; i < arr.length; i++) {
+     if(i % size === 0){
+        // Push a new array containing the current value to the res array
+        res.push([arr[i]]);
+     }
+     else{
+        // Push the current value to the current array
+        res[res.length-1].push(arr[i]);
+     };
+  };
+  return res;
+};
 let mp = new Map<string, Array<number>>([
   ['[10083]', [10083, 65039]],
   ['[9729]', [9729, 65039]],
@@ -137,21 +151,25 @@ export async function emojiMix(ctx: Context) {
       firstdeca = processDeca(firstdeca)
 
       let firstdecastr = firstdeca.map((c) => c.toString(16)).join('-')
-      if (cacheObject[firstdecastr]) {
-        let pairs = Object.keys(cacheObject[firstdecastr])
+
+      if (cacheObject[firstdecastr] || revCacheObject[firstdecastr]) {
+        let goodCache = cacheObject[firstdecastr]?cacheObject[firstdecastr]:revCacheObject[firstdecastr]
+        let pairs = Object.keys(goodCache)
         pairs = pairs.map((p) =>
           p
             .split('-')
             .map((c) => String.fromCodePoint(parseInt(c, 16)))
             .join('')
         )
-        return ctx.answerInlineQuery([
-          {
-            id: '0',
+        let groups = splitChunk(pairs,45)
+
+        return ctx.answerInlineQuery(groups.map((value,index)=>{
+          return {
+            id: `${index}`,
             type: 'article',
             title: 'Options(tap for all)',
             description:
-              'For ' + String.fromCodePoint(...firstdeca) + ': ' + pairs.join(),
+              'For ' + String.fromCodePoint(...firstdeca) + ': ' + value.join(),
             input_message_content: {
               message_text:
                 'For ' +
@@ -159,8 +177,40 @@ export async function emojiMix(ctx: Context) {
                 ': ' +
                 pairs.join(' '),
             },
-          },
-        ])
+          }
+        })
+
+
+
+        // return ctx.answerInlineQuery([
+        //   {
+        //     id: '0',
+        //     type: 'article',
+        //     title: 'Options(tap for all)',
+        //     description:
+        //       'For ' + String.fromCodePoint(...firstdeca) + ': ' + pairs.join(),
+        //     input_message_content: {
+        //       message_text:
+        //         'For ' +
+        //         String.fromCodePoint(...firstdeca) +
+        //         ': ' +
+        //         pairs.join(' '),
+        //     },
+        //   }, {
+        //     id: '1',
+        //     type: 'article',
+        //     title: 'O2ptions(tap for all)',
+        //     description:
+        //       'For ' + String.fromCodePoint(...firstdeca) + ': ' + pairs.join(),
+        //     input_message_content: {
+        //       message_text:
+        //         'For ' +
+        //         String.fromCodePoint(...firstdeca) +
+        //         ': ' +
+        //         pairs.join(' '),
+        //     },
+        //   },
+        // ])
       }
     }
 
